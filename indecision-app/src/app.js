069b22627743f5257077fa79c247f3fbe049bbd9
zweistein1326 <40302363+zweistein1326@users.noreply.github.com>
2020-05-05 15:41:44 +0530

    ES6 import/export

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,1 +1,5 @@
+import './utils';
+import { square, add } from './utils';
 console.log('app.js is running');
+console.log(square(9));
+console.log(add(45, 100));
---
+++ indecision-app/src/utils.js
@@ -0,0 +1,13 @@
+console.log('utils.js is running');
+
+const square = (x) => x * x;
+const add = (x,y) => x + y;
+
+// export { square, add };
+
+//exports - default export - named exports
+
+
+
+
+//exports- 
