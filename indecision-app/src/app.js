1e6b078467ed19102f2ab6c0eb608bbb7c3c99eb
zweistein1326 <40302363+zweistein1326@users.noreply.github.com>
2020-05-09 19:37:13 +0530

    redundant code deleted

--- indecision-app/src/app.js
+++
@@ -1,9 +0,0 @@
-// import validator from 'validator';
-
-// console.log(validator.isEmail('sid@gmail.com'));
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const template = React.createElement('p', {}, 'Testing 123'); //temporary
-ReactDOM.render(template, document.getElementById('app'));
--- indecision-app/src/person.js
+++
@@ -1,5 +0,0 @@
-const isAdult = (x) => { return x >= 18 };
-const canDrink = (x) => x >= 21;
-
-export { isAdult, canDrink };
-export default (x) => x > 60;
